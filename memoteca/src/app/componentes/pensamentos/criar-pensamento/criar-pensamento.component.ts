0b040b5f34149167da275722fd36bac1399e3157
Daniel Dos Santos Martins <danielsantosmartins.dev@gmail.com>
2023-08-06 12:57:15 -0300

    commit curso

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -14,7 +15,22 @@ export class CriarPensamentoComponent {
     autoria: '',
     modelo: 'modelo1',
   };
-  constructor(private service: PensamentoService, private router: Router) {}
+
+  formulario!: FormGroup;
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulário reativo'],
+      autoria: [''],
+      modelo: ['modelo1'],
+    });
+  }
 
   criarPensamento() {
     this.service.criar(this.pensamento).subscribe(() => {
